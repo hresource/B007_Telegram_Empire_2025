da26d0c07499982f3353df47fc2ed380377c12fd
hresource <hresource@my.com>
2025-11-08 03:25:24 -0800

    Delete B007_Telegram_Empire_2025 directory

--- B007_Telegram_Empire_2025/sms/__init__.py
+++
@@ -1,6 +0,0 @@
-# sms/__init__.py
-from .sms_activate import SMSActivateClient
-from .five_sim import FiveSimClient
-from .smspva import SMSPVAClient
-
-__all__ = ["SMSActivateClient", "FiveSimClient", "SMSPVAClient"]
